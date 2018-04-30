e06570d444bac43caf4828fbd220015450cf0011
Mehdi <mhalieager6@gmail.com>
2018-04-30 19:07:00 +0430

    Added assignment answer

--- app/src/main/java/me/mehdi/animationresources/MainActivity.java
+++ app/src/main/java/me/mehdi/animationresources/MainActivity.java
@@ -1,5 +1,6 @@
 package me.mehdi.animationresources;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button startButton;
     ImageView heartImage;
+    ConstraintLayout rootLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +22,18 @@ public class MainActivity extends AppCompatActivity {
 
         startButton = findViewById(R.id.start);
         heartImage = findViewById(R.id.heart);
+        rootLayout = findViewById(R.id.root);
 
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Animation animation1 = AnimationUtils.loadAnimation(MainActivity.this, R.anim.animate_one);
                 Animation animation2 = AnimationUtils.loadAnimation(MainActivity.this, R.anim.animate_two);
+                Animation rootAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.animate_root);
 
                 v.startAnimation(animation1);
                 heartImage.startAnimation(animation2);
+                rootLayout.startAnimation(rootAnim);
             }
         });
 
